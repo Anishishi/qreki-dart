40961cb151f4c187d16f3e26f4e641d1368a9d93
Anishishi <aki.nishi.work@gmail.com>
2024-04-16 22:05:04 +0900

    dart create package

---
+++ example/qreki_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:qreki_dart/qreki_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/qreki_dart_test.dart
@@ -0,0 +1,16 @@
+import 'package:qreki_dart/qreki_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
